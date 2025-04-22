970cda97fc2ecbb490134afb5c7f0bfc193da1d6
khaldmansour <khaldmansour93@gmail.com>
2025-04-22 16:46:30 +0200

    [user] search by status in products

--- app/Http/Controllers/API/V1/UserController.php
+++ app/Http/Controllers/API/V1/UserController.php
@@ -6,12 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductResource;
 use App\Http\Resources\RecentSearchResource;
 use App\Http\Resources\WantedProductResource;
+use App\Repositories\ProductRepository;
 use App\Repositories\RecentSearchRepository;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function __construct(private readonly RecentSearchRepository $recentSearchRepository)
+    public function __construct(private readonly RecentSearchRepository $recentSearchRepository, private readonly ProductRepository $productRepository, private readonly WantedProductResource $wantedProductResource)
     {
     }
 
@@ -78,10 +79,10 @@ class UserController extends Controller
 
     public function myWantedProducts(Request $request)
     {
-        $user = auth()->user();
         $limit = $request->input('limit', 10);
+        $wantedProducts = $this->wantedProductResource->where('user_id', auth()->user()->id)->paginate($limit);
 
-        return $this->success(WantedProductResource::collection($user->wantedProducts()->paginate($limit)));
+        return $this->success(WantedProductResource::collection($wantedProducts));
     }
 
     /**
@@ -147,15 +148,17 @@ class UserController extends Controller
 
     public function myProducts(Request $request)
     {
-        $user = auth()->user();
         $limit = $request->input('limit', 10);
 
-        return $this->success(ProductResource::collection($user->products()->paginate($limit)));
+        $products = $this->productRepository->where('user_id', auth()->user()->id)->paginate($limit);
+
+        return $this->success(ProductResource::collection($products));
     }
 
     public function myRecentSearches(Request $request)
     {
         $user = auth()->user();
+
         $recentSearches = $this->recentSearchRepository->where('user_id', $user->id)->limit(10)->get();
 
         return $this->success(RecentSearchResource::collection($recentSearches));
