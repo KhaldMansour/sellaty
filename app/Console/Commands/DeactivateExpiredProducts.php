7f81eef6a05d4614c1a69b1550e357c07204d3b7
khaldmansour <khaldmansour93@gmail.com>
2025-05-26 20:27:28 +0300

    [products] deactivate expired products

--- app/Console/Commands/DeactivateExpiredProducts.php
+++ app/Console/Commands/DeactivateExpiredProducts.php
@@ -14,20 +14,19 @@ class DeactivateExpiredProducts extends Command
     public function handle()
     {
         $this->info("Checking for expired products...");
-    
-        $expiredCount = 0; 
-    
+
+        $expiredCount = 0;
+
         Product::where('status', 'active')
             ->whereDate('listed_until', '<', now()->toDateString())
             ->chunkById(100, function ($products) use (&$expiredCount) {
                 $ids = $products->pluck('id')->toArray();
                 $expiredCount += count($ids);
-    
+
                 Product::whereIn('id', $ids)->update(['status' => 'inactive']);
             });
-    
+
         $this->info("Expired products have been deactivated.");
         $this->info("Total expired products: {$expiredCount}");
     }
-    
 }
