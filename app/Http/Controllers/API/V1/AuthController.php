815adbeffd97f999b9dec9eea0a65fad2a49734f
khaldmansour <khaldmansour93@gmail.com>
2025-05-27 13:18:49 +0300

    [otp] log response body

--- app/Http/Controllers/API/V1/AuthController.php
+++ app/Http/Controllers/API/V1/AuthController.php
@@ -163,7 +163,10 @@ class AuthController extends Controller
         $otpDriver = config('services.otp.driver', 'appsenders');
         $otpSender = OtpSenderFactory::create($otpDriver);
         $this->otpService->setOtpSender($otpSender);
-        $this->otpService->sendOtp($phoneNumber);
+        
+        if (!$this->otpService->sendOtp($phoneNumber)) {
+            return $this->failure('Failed to send OTP', 500);
+        };
 
         return $this->success(null, ['message' => 'OTP has been sent.'], 200);
     }
