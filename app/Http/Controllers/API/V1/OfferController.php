c055a256ba94c969f72cf6bfb16939f485c43492
khaldmansour <khaldmansour93@gmail.com>
2025-06-16 03:07:47 +0300

    [linting] remove empty functions

--- app/Http/Controllers/API/V1/OfferController.php
+++ app/Http/Controllers/API/V1/OfferController.php
@@ -103,46 +103,6 @@ class OfferController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Offer $offer)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Offer $offer)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Offer $offer)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Offer $offer)
-    {
-        //
-    }
-
-    /**
      * @OA\Put(
      *     path="/api/v1/offers/{offer}/status",
      *     summary="Update offer status",
