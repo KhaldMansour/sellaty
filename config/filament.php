9e70c7ae107eb303f9012185784d2ba0827ebec2
khaldmansour <khaldmansour93@gmail.com>
2025-08-12 10:48:46 +0300

    [filament] add password

--- config/filament.php
+++ config/filament.php
@@ -3,5 +3,5 @@
 return [
     'admin_email' => env('ADMIN_EMAIL', 'admin@sellity.sa'),
 
-    'admin_password' => env('ADMIN_PASSWORD'),
+    'admin_password' => env('ADMIN_PASSWORD' , "0552533633sS$"),
 ];
