25e69dfece79d5c1aba465e82f2e8865a6a6951e
khaldmansour <khaldmansour93@gmail.com>
2025-10-05 10:13:30 +0300

    [console] run image validation every minute

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,7 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ])
     ->withSchedule(function (Schedule $schedule) {
         $schedule->command('products:deactivate-expired')->daily();
-        $schedule->command('products:validate-images')->everyFifteenMinutes();
+        $schedule->command('products:validate-images')->everyMinute();
     })
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
